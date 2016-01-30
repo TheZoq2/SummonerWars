1c71963b8da90f585dba284bf1e299a64b51264e
Anders Dahnielson <anders@dahnielson.com>
2016-01-30 19:16:24 +0100

    Adding EffectLayer

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from cocos.actions import *
 
 from SpellWheel import *
 from BackgroundLayer import *
+from EffectLayer import *
 from Player import *
 
 cocos.director.director.init(width=Globals.VID_WIDTH, height=Globals.VID_HEIGHT, caption=Globals.GAME_NAME, fullscreen=Globals.VID_FULLSCREEN)
@@ -14,5 +15,6 @@ player = Player(sw)
 
 cocos.director.director.run(cocos.scene.Scene(
     BackgroundLayer(),
+    EffectLayer(),
     player.spellWheel
 ))
