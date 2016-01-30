3da82841a3d1e41762d55fa5a2204f702eabc7c3
= <frans.skarman@gmail.com>
2016-01-30 19:06:13 +0100

    Add onSelfCast event

--- test.py
+++ test.py
@@ -4,9 +4,15 @@ from cocos.actions import *
 
 from SpellWheel import *
 from Background import *
+from Player import *
 
 cocos.director.director.init(width=Globals.VID_WIDTH, height=Globals.VID_HEIGHT, caption=Globals.GAME_NAME, fullscreen=Globals.VID_FULLSCREEN)
+
+sw = SpellWheel(pyglet.input.get_joysticks()[0], (200, 200))
+
+player = Player(sw)
+
 cocos.director.director.run(cocos.scene.Scene(
     Background(),
-    SpellWheel(pyglet.input.get_joysticks()[0], (200, 200))
+    player.spellWheel
 ))
