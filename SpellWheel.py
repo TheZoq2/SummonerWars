cfa0c61ae1bb2040eceef678547b28b7fc82952d
Frans Skarman <frans.skarman@gmail.com>
2016-01-30 12:18:54 +0100

    ADd WIP spell wheel

--- SpellWheel.py
+++ SpellWheel.py
@@ -2,12 +2,16 @@ import cocos
 from cocos.actions import *
 import pyglet
 
+import math
+
 import Globals
 
 class SpellWheel(cocos.layer.Layer):
     BACKGROUND_IMAGE = "Assets/spellwheel_slice.png"
 
-    def __init__(self):
+    is_event_handler = True
+
+    def __init__(self, joystick):
         super( SpellWheel, self).__init__()
         self.ingredients = []
         
@@ -21,8 +25,40 @@ class SpellWheel(cocos.layer.Layer):
             self.backgrounds[i].rotation = angle
             self.backgrounds[i].position = 320,240
             self.backgrounds[i].image_anchor = 0,0
-            self.backgrounds[i].color = 255,0,0
+            self.backgrounds[i].color = 255,255,255
             self.add(self.backgrounds[i])
+
+        self.currentAngle = 0
+        self.currentSector = 0
+
+        
+        self.joystick = joystick
+        print(joystick.device)
+
+        self.joystick.open()
+
+        self.joystick.push_handlers(self)
+
+        
+    def on_joybutton_press(self, joystick, button):
+        print(button)
+    
+    def on_joyaxis_motion(self, joystick, axis, value):
+        #Calculate stick angle
+        x = joystick.rx
+        y = joystick.ry
+
+        angle = math.atan2(y, x) * 360/(2*math.pi) + 90
+
+        if(angle < 0): 
+            angle += 360
+
+        #Highlight the current sector
+        sector = math.floor(angle / 360 * Globals.INGREDIENTS_PER_TURN)
+
+        print(sector)
+
+        #self.backgrounds[sector].color = 255,0,0
 
 
         
