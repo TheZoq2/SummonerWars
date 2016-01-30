f0eb6fdb29d745b95734487900dc2d62362e6931
Frans Skarman <frans.skarman@gmail.com>
2016-01-30 13:35:02 +0100

    Add spell wheel location

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ from SpellWheel import *
 
 
 cocos.director.director.init(width=Globals.VID_WIDTH, height=Globals.VID_HEIGHT, caption=Globals.GAME_NAME, fullscreen=Globals.VID_FULLSCREEN)
-cocos.director.director.run (cocos.scene.Scene(SpellWheel(pyglet.input.get_joysticks()[0])))
+cocos.director.director.run (cocos.scene.Scene(SpellWheel(pyglet.input.get_joysticks()[0], (200, 200))))
 
