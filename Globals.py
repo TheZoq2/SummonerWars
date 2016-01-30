0975e916833b6a354e6bab56d529a3bcf628ba14
Anders Dahnielson <anders@dahnielson.com>
2016-01-30 13:27:16 +0100

    Adding screen resolution setting

--- Globals.py
+++ Globals.py
@@ -1,6 +1,12 @@
+GAME_NAME="Summoner Wars"
+
 #The amount of available ingredients that the player has for each spell
 INGREDIENTS_PER_TURN = 8
 
 #The amount of ingredients that exist in the game
 NUM_INGREDIENTS = 16
 
+# Video resolution
+VID_WIDTH=1280
+VID_HEIGHT=720
+VID_FULLSCREEN=False
--- test.py
+++ test.py
@@ -5,6 +5,6 @@ from cocos.actions import *
 from SpellWheel import *
 
 
-cocos.director.director.init()
+cocos.director.director.init(width=Globals.VID_WIDTH, height=Globals.VID_HEIGHT, caption=Globals.GAME_NAME, fullscreen=Globals.VID_FULLSCREEN)
 cocos.director.director.run (cocos.scene.Scene(SpellWheel(pyglet.input.get_joysticks()[0])))
 
