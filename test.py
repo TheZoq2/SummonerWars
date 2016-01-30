e02e2ebbee3b2eaa722bccfdb05e5fa7de0683bc
Anders Dahnielson <anders@dahnielson.com>
2016-01-30 16:40:48 +0100

    Adding background layer

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ import pyglet
 from cocos.actions import *
 
 from SpellWheel import *
-
+from Background import *
 
 cocos.director.director.init(width=Globals.VID_WIDTH, height=Globals.VID_HEIGHT, caption=Globals.GAME_NAME, fullscreen=Globals.VID_FULLSCREEN)
-cocos.director.director.run (cocos.scene.Scene(SpellWheel(pyglet.input.get_joysticks()[0], (200, 200))))
-
+cocos.director.director.run(cocos.scene.Scene(
+    Background(),
+    SpellWheel(pyglet.input.get_joysticks()[0], (200, 200))
+))
