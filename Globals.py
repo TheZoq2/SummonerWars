a7cce365e3331dd4e2a489c7d94a1ebebedc551e
= <frans.skarman@gmail.com>
2016-01-31 02:04:53 +0100

    Rename asset files

--- Globals.py
+++ Globals.py
@@ -42,23 +42,6 @@ RUNE_IMAGES = [
     "Assets/ingredients/rune8.png",
     "Assets/ingredients/rune9.png",
     "Assets/ingredients/rune10.png",
-    "Assets/ingredients/rune11.png",
-    "Assets/ingredients/rune12.png",
-    "Assets/ingredients/rune13.png",
-    "Assets/ingredients/rune14.png",
-    "Assets/ingredients/rune15.png",
-    "Assets/ingredients/rune16.png",
-    "Assets/ingredients/rune17.png",
-    "Assets/ingredients/rune18.png",
-    "Assets/ingredients/rune19.png",
-    "Assets/ingredients/rune20.png",
-    "Assets/ingredients/rune21.png",
-    "Assets/ingredients/rune22.png",
-    "Assets/ingredients/rune23.png",
-    "Assets/ingredients/rune24.png",
-    "Assets/ingredients/rune25.png",
-    "Assets/ingredients/rune26.png",
-    "Assets/ingredients/rune27.png"
 ]
 
 class UpdateAction( InstantAction ):
