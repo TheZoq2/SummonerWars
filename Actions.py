2c04ddb618499189c50a6dc7ac8eab613982c124
Anders Dahnielson <anders@dahnielson.com>
2016-01-31 16:37:07 +0100

    Moving custom actions into its own module

---
+++ Actions.py
@@ -0,0 +1,20 @@
+from cocos.actions import *
+
+class Remove(InstantAction):
+
+    def start(self):
+        self.target.kill()
+
+class ColorLerp(IntervalAction):
+
+    def init(self, from_color, to_color, duration):
+        super(ColorLerp, self).init(duration)
+        self.from_color = from_color
+        self.to_color = to_color
+        self.duration = duration
+
+    def update(self, t):
+        r = self.to_color[0] * t + self.from_color[0] * (1-t)
+        g = self.to_color[1] * t + self.from_color[1] * (1-t)
+        b = self.to_color[2] * t + self.from_color[2] * (1-t)
+        self.target.color = r,g,b
--- EffectLayer.py
+++ EffectLayer.py
@@ -1,32 +1,12 @@
 import Globals
 
 import cocos
-from cocos.actions import *
-from cocos.actions.interval_actions import *
 from cocos.text import Label
 from cocos.sprite import Sprite
 from SpellEffects import effectDispatchCenter
+from Actions import *
 
 from pyglet import font
-
-class Remove(InstantAction):
-
-    def start(self):
-        self.target.kill()
-
-class ColorLerp(IntervalAction):
-
-    def init(self, from_color, to_color, duration):
-        super(ColorLerp, self).init(duration)
-        self.from_color = from_color
-        self.to_color = to_color
-        self.duration = duration
-
-    def update(self, t):
-        r = self.to_color[0] * t + self.from_color[0] * (1-t)
-        g = self.to_color[1] * t + self.from_color[1] * (1-t)
-        b = self.to_color[2] * t + self.from_color[2] * (1-t)
-        self.target.color = r,g,b
 
 def Shoot(from_pos, to_pos, duration):
     return ( Place(from_pos) + Show() + MoveTo(to_pos, duration) + Hide() )
